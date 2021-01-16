dc1178e044e09f005fccf032b51f6d31446edc9c
Kris <krisgodom@gmail.com>
2021-01-16 14:10:49 -0400

    added two methods

--- src/services/CustomerService.java
+++ src/services/CustomerService.java
@@ -1,2 +1,32 @@
-package services;public class CustomerService {
+package services;
+
+import entities.Customer;
+import entities.Gender;
+import entities.Item;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CustomerService {
+
+    public List<Customer> getListOfFemaleCustomers (){
+        List<Customer> listOfFemaleCustomers = new ArrayList<>();
+        for (Customer cus: Customer.getCustomerList()) {
+            if(cus.getGender().equals(Gender.FEMALE)){
+                listOfFemaleCustomers.add(cus);
+            }
+        }
+        return listOfFemaleCustomers;
+    }
+
+    public List <Integer> getIdListOfFemalePurchases (){
+        List<Integer> idListOfFemalePurchases = new ArrayList<>();
+        for (Customer cus: getListOfFemaleCustomers()) {
+            for (int id: cus.getLastPurchases()) {
+                idListOfFemalePurchases.add(id);
+            }
+        }
+        return idListOfFemalePurchases;
+    }
+    
 }
