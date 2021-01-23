a565d368e5779466f787504ce0ad2aa7c4b39250
Kris <krisgodom@gmail.com>
2021-01-22 21:55:58 -0400

    added method readFromFile

--- src/FileUtils.java
+++ src/FileUtils.java
@@ -1,7 +1,22 @@
-import entities.Customer;
-
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
-   public List<Customer> readFromFile
+    public static List<String> readFromFile(Path path) {
+        List<String> dataFromFile = new ArrayList<>();
+        try (BufferedReader reader = Files.newBufferedReader(path)) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                dataFromFile.add(line);
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return dataFromFile;
+    }
 }
