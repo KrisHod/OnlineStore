067049fdddb9167b56632ef6835b28802fc508e1
KrisHod <krisgodom@gmail.com>
2021-02-21 20:51:24 -0400

    minor changes

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -3,14 +3,14 @@ package utils;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
-    public static List<String> readFromFile(Path path) {
+    public static List<String> readFromFile(String path) {
         List<String> dataFromFile = new ArrayList<>();
-        try (BufferedReader reader = Files.newBufferedReader(path)) {
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))){
             String line;
             while ((line = reader.readLine()) != null) {
                 dataFromFile.add(line);
