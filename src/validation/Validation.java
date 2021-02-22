96b22d198d70b2011864142742291b3f63e87e57
KrisHod <krisgodom@gmail.com>
2021-02-21 20:49:36 -0400

    refactored methods

--- src/validation/Validation.java
+++ src/validation/Validation.java
@@ -10,36 +10,24 @@ import java.time.format.DateTimeParseException;
 
 public class Validation {
 
-    public static boolean isValidNumericDataInString(String num) {
+    public static boolean isValidNumericDataInString(String num) throws FailedValidationException {
         // check if string contains only positive digitals
         if (!num.matches("^\\d+$")) {
-            try {
-                throw new FailedValidationException("Invalid input. It should contains only positive digitals");
-            } catch (FailedValidationException e) {
-                e.printStackTrace();
-                return false;
-
-
-            }
+            throw new FailedValidationException("Invalid input. It should contains only positive digitals");
         } else {
             return true;
         }
     }
 
-    public static boolean isValidStringLength(String str, int length) {
+    public static boolean isValidStringLength(String str, int length) throws FailedValidationException {
         if (str.length() > length) {
-            try {
-                throw new FailedValidationException("Length of string should be less than" + length + " character");
-            } catch (FailedValidationException e) {
-                e.printStackTrace();
-                return false;
-            }
+            throw new FailedValidationException("Length of string should be less than" + length + " character");
         } else {
             return true;
         }
     }
 
-    public static boolean isValidDateFormat(String date, DateTimeFormatter formatter) {
+    public static boolean isValidDateFormat(String date, DateTimeFormatter formatter) throws FailedValidationException {
         LocalDate ld = null;
 
         try {
@@ -47,12 +35,7 @@ public class Validation {
             String res = ld.format(formatter);
 
             if (!res.equals(date)) {
-                try {
-                    throw new FailedValidationException("Invalid format of date");
-                } catch (FailedValidationException e) {
-                    e.printStackTrace();
-                    return false;
-                }
+                throw new FailedValidationException("Invalid format of date");
             }
         } catch (DateTimeParseException e) {
             e.printStackTrace();
@@ -61,7 +44,7 @@ public class Validation {
         return true;
     }
 
-    public static boolean isValidDateTimeFormat(String date, DateTimeFormatter formatter) {
+    public static boolean isValidDateTimeFormat(String date, DateTimeFormatter formatter) throws FailedValidationException {
         LocalDateTime ldt = null;
 
         try {
@@ -69,12 +52,7 @@ public class Validation {
             String res = ldt.format(formatter);
 
             if (!res.equals(date)) {
-                try {
-                    throw new FailedValidationException("Invalid format of date and time");
-                } catch (FailedValidationException e) {
-                    e.printStackTrace();
-                    return false;
-                }
+                throw new FailedValidationException("Invalid format of date and time");
             }
         } catch (DateTimeParseException e) {
             e.printStackTrace();
@@ -83,29 +61,19 @@ public class Validation {
         return true;
     }
 
-    public static boolean isValidGender (Gender gender){
-        if (!( gender.equals(Gender.MALE) || gender.equals(Gender.FEMALE) || gender.equals(Gender.SOMETHING_ELSE))) {
-            try {
-                throw new FailedValidationException("Invalid value for \"Gender\"");
-            } catch (FailedValidationException e) {
-                e.printStackTrace();
-                return false;
-            }
+    public static boolean isValidGender(String gender) throws FailedValidationException {
+        if (!(gender.equals(Gender.MALE.toString()) || gender.equals(Gender.FEMALE.toString()) || gender.equals(Gender.SOMETHING_ELSE.toString()))) {
+            throw new FailedValidationException("Invalid value for \"Gender\"");
         } else {
             return true;
         }
     }
 
-    public static boolean isValidPhoneNumber (String number){
+    public static boolean isValidPhoneNumber(String number) throws FailedValidationException {
         if (number.matches("^((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$")) {
             return true;
         } else {
-            try {
-                throw new FailedValidationException("Invalid telephone number");
-            } catch (FailedValidationException e) {
-                e.printStackTrace();
-                return false;
-            }
+            throw new FailedValidationException("Invalid telephone number");
         }
     }
 }
