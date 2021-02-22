5e6229fbc6b860732a1fa079cf4bb79155dd182d
KrisHod <krisgodom@gmail.com>
2021-02-21 20:52:32 -0400

    minor changes

--- src/entities/Order.java
+++ src/entities/Order.java
@@ -6,24 +6,23 @@ import java.util.List;
 import java.util.Objects;
 
 public class Order {
-    private int number;
+    private int id;
     private Customer customer;
-    private List<Item> itemList = new ArrayList<>();
+    private List<Item> itemList;
     private LocalDate dateOrder;
 
-    public Order(int number, Customer customer, List<Item> itemList, LocalDate dateOrder) {
-        this.number = number;
+    public Order(Customer customer, List<Item> itemList, LocalDate dateOrder) {
         this.customer = customer;
         this.itemList = itemList;
         this.dateOrder = dateOrder;
     }
 
-    public int getNumber() {
-        return number;
+    public int getId() {
+        return id;
     }
 
-    public void setNumber(int number) {
-        this.number = number;
+    public void setId(int id) {
+        this.id = id;
     }
 
     public Customer getCustomer() {
@@ -55,18 +54,18 @@ public class Order {
         if (this == o) return true;
         if (!(o instanceof Order)) return false;
         Order order = (Order) o;
-        return getNumber() == order.getNumber() && Objects.equals(getCustomer(), order.getCustomer()) && Objects.equals(getItemList(), order.getItemList()) && Objects.equals(getDateOrder(), order.getDateOrder());
+        return getId() == order.getId() && Objects.equals(getCustomer(), order.getCustomer()) && Objects.equals(getItemList(), order.getItemList()) && Objects.equals(getDateOrder(), order.getDateOrder());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getNumber(), getCustomer(), getItemList(), getDateOrder());
+        return Objects.hash(getId(), getCustomer(), getItemList(), getDateOrder());
     }
 
     @Override
     public String toString() {
         return "Order{" +
-                "number=" + number +
+                "id=" + id +
                 ", customer=" + customer +
                 ", itemList=" + itemList +
                 ", dateOrder=" + dateOrder +
