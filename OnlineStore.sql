f60166d5b4ccccce9529121ee7296157d9f249fe
KrisHod <krisgodom@gmail.com>
2021-02-24 22:45:41 -0400

    added MySQL script

---
+++ OnlineStore.sql
@@ -0,0 +1,39 @@
+CREATE TABLE customer
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    doB DATE NOT NULL,
+    address VARCHAR(60) NOT NULL,
+    gender VARCHAR(20) NOT NULL,
+    phoneNumber VARCHAR(20),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE item
+(
+    id INT NOT NULL,
+    title VARCHAR(50) NOT NULL,
+    code INT NOT NULL,
+    producer VARCHAR(50),
+    dateOfLastUpdate DATE NOT NULL,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE `order`
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    customerId INT NOT NULL,
+    date DATE NOT NULL,
+    FOREIGN KEY cust_fk (customerId) REFERENCES customer (id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE orderedItems
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    orderId INT NOT NULL,
+    itemId INT NOT NULL,
+    FOREIGN KEY order_fk (orderId) REFERENCES `order` (id),
+    FOREIGN KEY item_fk (itemId) REFERENCES item (id),
+    PRIMARY KEY (id)
+);
---
+++ src/fileReader/Constants.java
@@ -0,0 +1,18 @@
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+
+public final class Consts {
+    private static final Locale locale = new Locale("en");
+    public static final DateTimeFormatter CUSTOMER_DOB = DateTimeFormatter.ofPattern("d MMMM yyyy", locale);
+    public static final DateTimeFormatter DATE_OF_LAST_PURCHASE = DateTimeFormatter.ofPattern("M/dd/yyyy", locale);
+    public static final DateTimeFormatter DATE_OF_LAST_PURCHASE2 = DateTimeFormatter.ofPattern("MM.dd.yyyy", locale);
+    public static final DateTimeFormatter DATE_OF_LAST_UPDATE = DateTimeFormatter.ofPattern("dd.MM.yyyy H:mm");
+    public static final String ITEMS_PATH = "C:\\Users\\Zver\\Desktop\\Kris\\Go2It\\OnlineStore\\src\\utils\\items.csv";
+    public static final String CUSTOMERS_PATH = "C:\\Users\\Zver\\Desktop\\Kris\\Go2It\\OnlineStore\\src\\utils\\Customers.csv";
+
+    private Consts(){
+        //this prevents even the native class from
+        //calling this constructor as well :
+        throw new AssertionError();
+    }
+}
