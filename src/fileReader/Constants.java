efe91c95533ca4844cbd4db7bc52281e46e1e95f
KrisHod <krisgodom@gmail.com>
2021-02-26 21:50:03 -0400

    cache implementation

--- src/fileReader/Constants.java
+++ src/fileReader/Constants.java
@@ -1,7 +1,9 @@
+package fileReader;
+
 import java.time.format.DateTimeFormatter;
 import java.util.Locale;
 
-public final class Consts {
+public final class Constants {
     private static final Locale locale = new Locale("en");
     public static final DateTimeFormatter CUSTOMER_DOB = DateTimeFormatter.ofPattern("d MMMM yyyy", locale);
     public static final DateTimeFormatter DATE_OF_LAST_PURCHASE = DateTimeFormatter.ofPattern("M/dd/yyyy", locale);
@@ -10,7 +12,7 @@ public final class Consts {
     public static final String ITEMS_PATH = "C:\\Users\\Zver\\Desktop\\Kris\\Go2It\\OnlineStore\\src\\utils\\items.csv";
     public static final String CUSTOMERS_PATH = "C:\\Users\\Zver\\Desktop\\Kris\\Go2It\\OnlineStore\\src\\utils\\Customers.csv";
 
-    private Consts(){
+    private Constants(){
         //this prevents even the native class from
         //calling this constructor as well :
         throw new AssertionError();
--- src/fileReader/ItemFileReader.java
+++ src/fileReader/ItemFileReader.java
@@ -1,2 +1,71 @@
-package fileReader;public class ItemFileReader {
+package fileReader;
+
+import entities.Item;
+import exceptions.FailedValidationException;
+import validation.Validator;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import static utils.FileUtils.readFromFile;
+
+public class ItemFileReader {
+    private List<Item> items = getAll(Constants.ITEMS_PATH);
+
+    public Map<String, List<Item>> pathToCustomersList = Map.of(Constants.ITEMS_PATH, items);
+
+    public List<Item> getItems() {
+        return items;
+    }
+
+    public void setItems(List<Item> items) {
+        this.items = items;
+    }
+
+    public List<Item> getAll(String path) {
+        List<Item> items = new ArrayList<>();
+        List<String> dataList = readFromFile(path);
+        int id = 0;
+        String title = null;
+        int code = 0;
+        String producer = null;
+        LocalDateTime dateOfLastUpdate = null;
+
+        try {
+            for (int i = 1; i < dataList.size(); i++) {
+                String[] array = dataList.get(i).split(";");
+
+                //id validation
+                if (Validator.isValidNumericDataInString(array[0])) {
+                    id = Integer.parseInt(array[0]);
+                }
+
+                //title validation
+                if (Validator.isValidStringLength(array[1], 50)) {
+                    title = array[1];
+                }
+
+                //code validation
+                if (Validator.isValidNumericDataInString(array[2])) {
+                    code = Integer.parseInt(array[2]);
+                }
+
+                //producer validation
+                if (Validator.isValidStringLength(array[3], 50)) {
+                    producer = array[3];
+                }
+
+                //date and time format validation
+                if (Validator.isValidDateTimeFormat(array[4], Constants.DATE_OF_LAST_UPDATE)) {
+                    dateOfLastUpdate = LocalDateTime.parse(array[4], Constants.DATE_OF_LAST_UPDATE);
+                }
+                items.add(new Item(id, title, code, producer, dateOfLastUpdate));
+            }
+        } catch (FailedValidationException e) {
+            e.printStackTrace();
+        }
+        return items;
+    }
 }
