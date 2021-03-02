561ed0ca7e627de06a9af344962ad84c4a1355a4
KrisHod <krisgodom@gmail.com>
2021-03-02 11:54:25 -0400

    added OrderRepository class

--- src/repository/OrderRepository.java
+++ src/repository/OrderRepository.java
@@ -1,2 +1,28 @@
-package repository;public class OrderRepository {
+package repository;
+
+import entities.Customer;
+import entities.Order;
+import utils.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+
+public class OrderRepository {
+    CustomerRepository customerRepository = new CustomerRepository();
+
+    public void add(Order order) {
+        String sql = "INSERT INTO order (customerId, date) values(?, ?)";
+
+        try (Connection con = DBUtil.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+            stmt.setInt(1, customerRepository.getId(order.getCustomer()));
+            stmt.setTimestamp(2, Timestamp.valueOf(order.getDateOrder().atStartOfDay()));
+
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        }
+    }
 }
