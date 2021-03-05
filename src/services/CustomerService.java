76b42c628d541c17184f001c4f32450ed05cbe93
KrisHod <krisgodom@gmail.com>
2021-03-05 15:19:28 -0400

    minor changes

--- src/services/CustomerService.java
+++ src/services/CustomerService.java
@@ -1,38 +1,51 @@
 package services;
 
 import entities.Customer;
+import entities.Gender;
 import fileReader.CustomerFileReader;
+import repository.CustomerRepository;
 
+import java.util.List;
 
 public class CustomerService {
-CustomerFileReader customerFileReader = new CustomerFileReader();
+    private CustomerFileReader customerFileReader;
+    private CustomerRepository customerRepository;
 
-    public Customer getByName(String name) {
-        for (Customer cus : customerFileReader.getCustomers()) {
+    public CustomerService() {
+        init();
+    }
+
+    private void init() {
+        this.customerFileReader = new CustomerFileReader();
+        this.customerRepository = new CustomerRepository();
+    }
+
+    public void addAllToDB(List<Customer> customers) {
+        for (Customer cus : customers) {
+            if (!isInDB(cus)) {
+                customerRepository.add(cus);
+            }
+        }
+    }
+
+    public boolean isInDB(Customer customer) {
+        return customer.equals(customerRepository.getById(customer.getId()));
+    }
+
+    public Customer getByName(String name, String path) {
+        for (Customer cus : customerFileReader.getAll(path)) {
             if (cus.getName().equals(name)) {
                 return cus;
             }
         }
         return null;
     }
-}
 
-//    public List<Customer> getListOfCustomersByGender(Gender gender) {
-//        List<Customer> customerList = new ArrayList<>();
-//        for (Customer cus : Customer.getCustomerList()) {
-//            if (cus.getGender().equals(gender)) {
-//                customerList.add(cus);
-//            }
-//        }
-//        return customerList;
-//    }
-//
-//    public List<Integer> getIdListOfPurchasesByGender(Gender gender) {
-//        List<Integer> idListOfFemalePurchases = new ArrayList<>();
-//        for (Customer cus : getListOfCustomersByGender(gender)) {
-//            for (int id : cus.getLastPurchases()) {
-//                idListOfFemalePurchases.add(id);
-//            }
-//        }
-//        return idListOfFemalePurchases;
-//    }
+    public List<Customer> getAll() {
+        return customerRepository.getAll();
+    }
+
+    public List<Customer> getByGender(Gender gender) {
+        return customerRepository.getByGender(gender);
+    }
+}
