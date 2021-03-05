63757cce0556c05b5b412af71dff57660fcdccd3
KrisHod <krisgodom@gmail.com>
2021-03-04 23:18:47 -0400

    added getByGender

--- src/repository/CustomerRepository.java
+++ src/repository/CustomerRepository.java
@@ -6,25 +6,27 @@ import utils.DBUtil;
 
 import java.sql.*;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class CustomerRepository {
     public void add(Customer customer) {
-            String sql = "INSERT INTO customer (name, doB, address, gender, phoneNumber) values(?, ?, ?, ?, ?)";
+        String sql = "INSERT INTO customer (name, doB, address, gender, phoneNumber) values(?, ?, ?, ?, ?)";
 
-            try (Connection con = DBUtil.getConnection();
-                 PreparedStatement stmt = con.prepareStatement(sql)) {
-                stmt.setString(1, customer.getName());
-                stmt.setTimestamp(2, Timestamp.valueOf(customer.getDoB().atStartOfDay()));
-                stmt.setString(3, customer.getAddress());
-                stmt.setString(4, String.valueOf(customer.getGender()));
-                stmt.setString(5, customer.getPhoneNumber());
+        try (Connection con = DBUtil.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+            stmt.setString(1, customer.getName());
+            stmt.setTimestamp(2, Timestamp.valueOf(customer.getDoB().atStartOfDay()));
+            stmt.setString(3, customer.getAddress());
+            stmt.setString(4, String.valueOf(customer.getGender()));
+            stmt.setString(5, customer.getPhoneNumber());
 
-                stmt.executeUpdate();
-            } catch (SQLException ex) {
-                System.out.println("Error " + ex.getMessage());
-            }
+            stmt.executeUpdate();
+        } catch (SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
         }
+    }
 
     public Integer getId(Customer customer) {
         int id = 0;
@@ -65,5 +67,36 @@ public class CustomerRepository {
             System.out.println("Error " + ex.getMessage());
         }
         return customer;
+    }
+
+    public List<Customer> getByGender(Gender gender) {
+        List<Customer> customers = new ArrayList<>();
+        String sql = "SELECT * FROM customer WHERE gender='" + gender.toString() + "'";
+        try (Connection con = DBUtil.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                customers.add(getCustomer(rs));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        }
+        return customers;
+    }
+
+    public List<Customer> getAll() {
+        List<Customer> customers = new ArrayList<>();
+        String sql = "SELECT * FROM customer ";
+
+        try (Connection con = DBUtil.getConnection();
+             PreparedStatement stmt = con.prepareStatement(sql)) {
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next()) {
+                customers.add(getCustomer(rs));
+            }
+        } catch (SQLException ex) {
+            System.out.println("Error " + ex.getMessage());
+        }
+        return customers;
     }
 }
