ace0fa6b9b54bb4fdcb1b2ceb743ca7061d5dea6
KrisHod <krisgodom@gmail.com>
2021-03-05 15:24:50 -0400

    added write method

--- src/utils/FileUtils.java
+++ src/utils/FileUtils.java
@@ -1,16 +1,20 @@
 package utils;
 
+import entities.Item;
+
 import java.io.BufferedReader;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileUtils {
     public static List<String> readFromFile(String path) {
         List<String> dataFromFile = new ArrayList<>();
-        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))){
+        try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
             String line;
             while ((line = reader.readLine()) != null) {
                 dataFromFile.add(line);
@@ -20,5 +24,32 @@ public class FileUtils {
             e.printStackTrace();
         }
         return dataFromFile;
+    }
+
+    public void writeItems(List<Item> items, String fileName) {
+
+        try (FileWriter csvWriter = new FileWriter(fileName)) {
+            csvWriter.append("id");
+            csvWriter.append(";");
+            csvWriter.append("title");
+            csvWriter.append(";");
+            csvWriter.append("code");
+            csvWriter.append(";");
+            csvWriter.append("producer");
+            csvWriter.append(";");
+            csvWriter.append("dateOfLastUpdate");
+            csvWriter.append(";");
+            csvWriter.append("\n");
+
+            for (Item row : items) {
+                csvWriter.append(String.join(";", String.valueOf(row.getId()), row.getTitle(),
+                        String.valueOf(row.getCode()), row.getProducer(),
+                        row.getDateOfLastUpdate().format(DateTimeFormatter.ofPattern("dd.MMMM yyyy"))));
+                csvWriter.append("\n");
+            }
+            csvWriter.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
